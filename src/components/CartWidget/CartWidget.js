7d61b7ed220da625e7cfbaba82baea47d7d263d4
FM / Ramiro Vechiola (Gerencia de Tecnología - TIC) <ramiro.vechiola@fm.gob.ar>
2022-10-28 16:27:04 -0300

    ClickEnCarrito

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -8,21 +8,30 @@ import carritoLleno from './carritoLleno.png';
 import {useCallback} from 'react';
 import {useNavigate} from 'react-router-dom';
 
+import Swal from 'sweetalert2'
+
 function SeccionCarrito() {
 
     const { contador } = useContext(CarritoContext);
  
     const navigate = useNavigate();
     const handleOnClick = useCallback(() => navigate('/carrito', {replace: true}), [navigate]);
-    
 
+    const carroVacio = () => {
+        Swal.fire({
+            title: 'Carrito Vacio',
+            text: 'Nada agregado aún !!',
+            icon: 'warning',
+        });
+    };
+    
     return (
         <div className="cartWidget">
             <div className="contador">
                 <p>{contador}</p>
             </div>
             <div className="carro">
-                <img src={contador > 0 ? carritoLleno : carritoVacio} alt="Carrito" onClick = { handleOnClick }></img>
+                <img src={contador > 0 ? carritoLleno : carritoVacio} alt="Carrito" onClick = { contador !== 0 ? handleOnClick : carroVacio }></img>
             </div>
         </div>
     );
